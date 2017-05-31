bf42f49f7c51ae3c3f2007b7d27257b6ecd6790a
Pascal Peridont <pperidont@motion-twin.com>
2017-05-31 11:56:23 +0200

    Added basic bytecode embedding (windows)

--- src/std/sys.c
+++ src/std/sys.c
@@ -591,7 +591,7 @@ HL_PRIM void hl_sys_init(void **args, int nargs, void *hlfile) {
 }
 
 HL_PRIM vbyte *hl_sys_hl_file() {
-	return hl_file;
+	return hl_file!=NULL ? hl_file : hl_sys_exe_path();
 }
 
 #ifndef HL_MOBILE
