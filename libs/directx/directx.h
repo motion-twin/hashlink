d3192b336d85d7283cdbea85c3c24a1563e67088
crautou <crautou@motion-twin.fr>
2018-01-04 11:02:07 +0100

    Add missing include on xbo platform

---
+++ libs/directx/directx.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef DIRECTX_H
+#define DIRECTX_H
+
+typedef struct dx_driver_ {
+    ID3D11Device *device;
+    ID3D11DeviceContext *context;
+    IDXGISwapChain *swapchain;
+    ID3D11RenderTargetView *renderTarget;
+    D3D_FEATURE_LEVEL feature;
+    int init_flags;
+} dx_driver;
+
+#endif
